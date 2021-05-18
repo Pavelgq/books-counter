b043cf50f39cdc325842156cbe69a56275bcb58a
Pavel <pavelgq@yandex.ru>
2021-05-18 20:27:57 +0300

    add useFetch hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+import useFetch from './hooks/useFetch'
+
 function App() {
+  const data = useFetch('https://www.googleapis.com/books/v1/volumes?q=php&key=AIzaSyA7qptRezeN4YlCDe47CRcDN8XMNpTG0P8', {})
+  console.log(data)
   return (
     <div className="App">
       <header className="App-header">
