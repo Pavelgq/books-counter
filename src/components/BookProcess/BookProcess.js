f7e53ce4005a62d4a3490ad930dc439e787997ff
Pavel <pavelgq@yandex.ru>
2021-06-15 22:27:31 +0300

    delete books, count pages and save in localstorage

--- src/components/BookProcess/BookProcess.js
+++ src/components/BookProcess/BookProcess.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 
 import styles from './BookProcess.module.css'
 
-const BookProcess = ({data}) => {
-    const [complitePages, setComplitePages] = useState(0)
+const BookProcess = ({data, handleDelete, handlePages}) => {
+    const [complitePages, setComplitePages] = useState(data.readPageCount || 0)
     
     const imageLink = data.imageLinks 
         || 'https://st4.depositphotos.com/2381417/26959/i/600/depositphotos_269592714-stock-photo-no-thumbnail-image-placeholder-for.jpg'
 
-    const allPages = 1000;
+    const allPages = data.pageCount || 1000;
 
     const fixTitle = (title, length) => {
         let newTitle = title;
@@ -20,21 +20,26 @@ const BookProcess = ({data}) => {
 
     const handleChange = (event) => {
         setComplitePages(event.target.value)
+        handlePages(data.title, event.target.value)
     }
     const handleClick = () => {
         console.log('delete book from library')
+        handleDelete(data.title)
     }
     
     const persent = Math.floor((complitePages / allPages) * 100)
     return (
         <>
-            <a href='#' className={styles.link}>
-                <img 
-                    className={styles.image} 
-                    src={imageLink.thumbnail}
-                    alt={data.title}
-                ></img>
-            </a>
+            <div className={styles.container}>
+                <a href='#' className={styles.link}>
+                    <img 
+                        className={styles.image} 
+                        src={imageLink.thumbnail}
+                        alt={data.title}
+                    ></img>
+                </a>
+                <button type='button' className={styles.button} onClick={handleClick}>X</button>
+            </div>
             
             <div className={styles.container}>
                 <h3 className={styles.title}>{fixTitle(data.title, 18)}</h3>
@@ -50,7 +55,7 @@ const BookProcess = ({data}) => {
                     <span>Процент: </span> 
                     <span>{persent}%</span>
                 </p>
-                <button type='button' className={styles.button} onClick={handleClick}>Удалить</button>
+                
             </div>
             
            
--- src/pages/library/library.js
+++ src/pages/library/library.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import useLocalStorage from '../../hooks/useLocalStorage'
 
@@ -8,24 +8,41 @@ import styles from './library.module.css'
 const Library = () => {
     const [myBooks, setBooks] = useLocalStorage('library')
     console.log(myBooks)
-    const handleDelete = (id) => {
+    const handleDelete = (title) => {
         //find myBooks with id and delete from localstorage array
-        const index = myBooks.some((element, index) => {
-            if (element.id === id) {
-                return index
+        const index = myBooks.findIndex((element, index) => {
+            console.log(element.title, title)
+            if (element.title === title) {
+                return true
             }
         })
-
-        // const newLibrary = myBooks.slice()
-        // myBooks.splice(index, 1)
+        const newLibrary = myBooks.slice()
+        newLibrary.splice(index, 1)
+        setBooks(newLibrary)
+        return newLibrary
     }
+
+    const handlePages = (title, page) => {
+        const index = myBooks.findIndex((element, index) => {
+            console.log(element.title, title)
+            if (element.title === title) {
+                return true
+            }
+        })
+        const newLibrary = myBooks.slice()
+        newLibrary[index].readPageCount = page;
+        setBooks(newLibrary)
+        return newLibrary
+    }
+
     const cards = myBooks && myBooks.map((book) => 
         (<li className={styles.list}>
-            <BookProcess data={book} key={book.id} handleDelete={handleDelete}></BookProcess>
+            <BookProcess data={book} key={book.id} handleDelete={handleDelete} handlePages={handlePages}></BookProcess>
         </li>))
+
     return (
         <div className={styles.container}>
-            <h2>Books</h2>
+            <h2 className={styles.title}>Сейчас читаю</h2>
             <ul className={styles.grid}>
                  {cards}
             </ul>
